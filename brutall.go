0fe597a33bc8cb54819e089c0780f89fbb9e64fe
Glen Baker <iepathos@gmail.com>
2017-12-10 14:03:49 -0800

    added build instructions to brutall.go for volumes and services

--- brutall.go
+++ brutall.go
@@ -4,8 +4,17 @@ import (
 	log "github.com/sirupsen/logrus"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
+func getBaseDir() string {
+	ex, err := os.Executable()
+	if err != nil {
+		panic(err)
+	}
+	exPath := filepath.Dir(ex)
+	return exPath
+}
 
 func gatherTools() {
 	// ./services/clone_tools.sh
@@ -15,10 +24,53 @@ func gatherTools() {
 	if err := exec.Command(cmd, args...).Run(); err != nil {
 		log.WithFields(log.Fields{
 			"docker": "localhost",
-			"error": err.Error(),
+			"error":  err.Error(),
 		}).Error("An error occurred trying to gather the necessary tools")
 		os.Exit(1)
 	}
+}
+
+func buildService(servicePath string) {
+	os.Chdir(servicePath)
+	cmd := "./build.sh"
+	args := []string{}
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		log.WithFields(log.Fields{
+			"docker":  "localhost",
+			"error":   err.Error(),
+			"service": servicePath,
+		}).Error("An error occurred trying to build")
+		os.Exit(1)
+	}
+}
+
+func buildVolumes() {
+	// go into volumes and build.sh
+	log.WithFields(log.Fields{"docker": "localhost"}).Info("Building volumes")
+	baseDir := getBaseDir()
+	volumesDir := filepath.Join(baseDir, "volumes")
+	buildService(volumesDir)
+}
+
+func buildServices() {
+	// go into services repos and build.sh
+	log.WithFields(log.Fields{"docker": "localhost"}).Info("Building services")
+	baseDir := getBaseDir()
+	servicesDir := filepath.Join(baseDir, "services")
+	services := []string{
+		"gobuster",
+		"sublist3r",
+		"altdns",
+	}
+	for _, service := range services {
+		servicePath := filepath.Join(servicesDir, service)
+		buildService(servicePath)
+	}
+}
+
+func build() {
+	buildVolumes()
+	buildServices()
 }
 
 func runService(service string, domain string) {
@@ -28,23 +80,17 @@ func runService(service string, domain string) {
 	args := []string{"-d", domain, "-t", string(threads), "-v"}
 	if err := exec.Command(cmd, args...).Run(); err != nil {
 		log.WithFields(log.Fields{
-			"docker": "localhost",
-			"error": err.Error(),
+			"docker":  "localhost",
+			"error":   err.Error(),
 			"service": service,
 		}).Error("An error occurred trying to execute")
 		os.Exit(1)
 	}
 }
 
-
 func main() {
 	log.WithFields(log.Fields{"docker": "localhost"}).Info("Starting one brave binary!")
 
 	gatherTools()
-
-	log.WithFields(log.Fields{"docker": "gobuster"}).Info("Starting gobuster")
-
-	log.WithFields(log.Fields{"docker": "sublist3r"}).Info("Starting sublist3r")
-
-	log.WithFields(log.Fields{"docker": "altdns"}).Info("Starting altdns")
+	build()
 }
