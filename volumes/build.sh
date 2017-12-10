368c20ae0fe49b31cfa5aab4be2dea79d02cb4d3
Glen Baker <iepathos@gmail.com>
2017-12-10 13:26:33 -0800

    added merge_words to build

--- volumes/build.sh
+++ volumes/build.sh
@@ -4,6 +4,10 @@
 docker volume create words
 
 # add and .txt files in words/ to the words volume
+pushd words
+	echo "Merge words lists into allwords.txt"
+	./merge_words.sh > allwords.txt
+popd
 
 for wordfile in words/*.txt ; do
 	echo "Adding file $wordfile to words docker volume"
