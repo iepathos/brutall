6b71976c4a563f221fe9dda78788b355ff6c9b26
Glen Baker <iepathos@gmail.com>
2017-12-10 02:24:12 -0800

    starting to build out brutall.go main script, making clone tools smarter

--- brutall.go
+++ brutall.go
@@ -2,8 +2,49 @@ package main
 
 import (
 	log "github.com/sirupsen/logrus"
+	"os"
+	"os/exec"
 )
 
+
+func gatherTools() {
+	// ./services/clone_tools.sh
+	log.WithFields(log.Fields{"docker": "localhost"}).Info("Gathering tools from github")
+	cmd := "./services/clone_tools.sh"
+	args := []string{}
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		log.WithFields(log.Fields{
+			"docker": "localhost",
+			"error": err.Error(),
+		}).Error("An error occurred trying to gather the necessary tools")
+		os.Exit(1)
+	}
+}
+
+func runService(service string, domain string) {
+	// results <- runService("sublist3r")
+	threads := 100
+	cmd := "./services/" + service + "/" + service + ".sh"
+	args := []string{"-d", domain, "-t", threads, "-v"}
+	if err := exec.Command(cmd, args...).Run(); err != nil {
+		log.WithFields(log.Fields{
+			"docker": "localhost",
+			"error": err.Error(),
+			"service": service,
+		}).Error("An error occurred trying to execute")
+		os.Exit(1)
+	}
+}
+
+
 func main() {
-	log.WithFields(log.Fields{"docker": "host"}).Info("One brave binary!")
+	log.WithFields(log.Fields{"docker": "localhost"}).Info("Starting one brave binary!")
+
+	gatherTools()
+
+	log.WithFields(log.Fields{"docker": "gobuster"}).Info("Starting gobuster")
+
+	log.WithFields(log.Fields{"docker": "sublist3r"}).Info("Starting sublist3r")
+
+	log.WithFields(log.Fields{"docker": "altdns"}).Info("Starting altdns")
 }
--- services/clone_tools.sh
+++ services/clone_tools.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-git clone https://github.com/iepathos/gobuster
-git clone https://github.com/iepathos/sublist3r
-git clone https://github.com/iepathos/altdns
+if [ ! -d $SCRIPTPATH/gobuster ]; then
+	git clone https://github.com/iepathos/gobuster $SCRIPTPATH/gobuster
+fi
+if [ ! -d $SCRIPTPATH/sublist3r ]; then
+	git clone https://github.com/iepathos/sublist3r $SCRIPTPATH/sublist3r
+fi
+if [ ! -d $SCRIPTPATH/altdns ]; then
+	git clone https://github.com/iepathos/altdns $SCRIPTPATH/altdns
+fi
