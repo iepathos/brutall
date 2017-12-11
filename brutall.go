9c2cdff1555444cfded9967bff0ba9f23a3a4a86
Glen Baker <iepathos@gmail.com>
2017-12-10 16:38:48 -0800

    cleanup

--- brutall.go
+++ brutall.go
@@ -110,7 +110,7 @@ func runServices(domain string) {
 
 func main() {
 	if len(os.Args) < 2 {
-		log.WithFields(log.Fields{}).Info("Usage: ./brutall example.domain.com")
+		log.WithFields(log.Fields{}).Info("Usage: ./brutall domain.com")
 		os.Exit(1)
 	} else {
 		log.WithFields(log.Fields{}).Info("Starting one brave binary!")
